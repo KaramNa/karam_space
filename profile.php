2fc1cc764902c4d121083d2d913c04e00da9ea4a
karam nassar <karam.nassar@hotmail.com>
2021-06-27 11:28:45 +0300

    just adding a file

--- profile.php
+++ profile.php
@@ -112,11 +112,88 @@ $user = $query->fetch_assoc();
             </div>
         </div>
         <div class="row mt-5">
-            <p>Full Name: <b class="text-capitalize"><?php echo $user["firstname"] . " " . $user["lastname"] ?></b></p>
-            <p>Email Address: <b class="text-capitalize"><?php echo $user["email"] ?></b></p>
-            <p>Gender: <b class="text-capitalize"><?php echo $user["gender"] ?></b></p>
-            <p>Birthday: <b class="text-capitalize"><?php echo $user["birthday"] ?></b></p>
+            <div class="col-md-4">
+                <p>Full Name: <b class="text-capitalize"><?php echo $user["firstname"] . " " . $user["lastname"] ?></b></p>
+                <p>Email Address: <b class="text-capitalize"><?php echo $user["email"] ?></b></p>
+                <p>Gender: <b class="text-capitalize"><?php echo $user["gender"] ?></b></p>
+                <p>Birthday: <b class="text-capitalize"><?php echo $user["birthday"] ?></b></p>
+            </div>
+            <div class="col-md-8">
+                <?php $request_to_id = $_SESSION["user_id"];
+                $query = $con->query("SELECT * FROM users JOIN posts ON users.user_id=$request_to_id AND posts.user_id=$request_to_id ORDER BY post_id DESC");
+                while ($row = $query->fetch_assoc()) {
+                    $posted_by = $row["firstname"] . " " . $row["lastname"];
+                    $post_image = $row["post_image"];
+                    $user_image = $row["profile_picture"];
+                    $content = $row["post_content"];
+                    $post_id = $row["post_id"];
+                    $post_date = $row["post_date"];
+                ?>
+                    <!-- show posts -->
+                    <div class="border p-3 mb-3">
+                        <div class="row">
+                            <div class="col-sm-2">
+                                <div class=""><img class="img-fluid rounded-circle" src="<?php echo $user_image ?>" alt=""></div>
+                            </div>
+                            <div class="col-sm-9 d-flex flex-column justify-content-center">
+                                <div class="text-capitalize"><?php echo $posted_by ?></div>
+                                <span><?php echo $post_date ?></span>
+                            </div>
+                            <div class="col-sm-1 ps-2"><a href="delete_post.php<?php echo '?id=' . $post_id ?>" class="nav-link text-secondary">X</a></div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="row my-4 ps-4"><?php echo $content ?></div>
+                            <div class="row"><img class="img-fluid" src="<?php echo $post_image ?>" alt=""></div>
+                        </div>
+                        <div class="row mt-3">
+                            <?php
+                            $comment = $con->query("SELECT * FROM comments WHERE post_id = '$post_id' ORDER BY post_id DESC");
+                            while ($row = $comment->fetch_assoc()) {
+                                $comment_id = $row["comment_id"];
+                                $comment_content = $row["comment_content"];
+                                $comment_date = $row["comment_date"];
+                            ?>
+                                <!-- show comments -->
+                                <div class="row mt-1">
+                                    <div class="col-lg-1 col-md-2"><img class="img-fluid img-comment" src="<?php echo $user_image ?>" alt=""></div>
+                                    <div class="col-lg-11 col-md-10">
+                                        <div class="bg-light rounded p-1">
+                                            <p class="mb-0 text-capitalize"><strong><?php echo $posted_by ?></strong></p>
+                                            <div id="new_comment" class="d-none" data-id="<?php echo "div" . $comment_id ?>">
+                                                <textarea class="form-control" id="new_comment_content" rows="2" placeholder="Enter a comment" data-id="<?php echo "textarea" . $comment_id ?>"></textarea>
+                                                <button class="link-button small" onclick="edit_done()">Done</button>
+                                                <button class="link-button small" onclick="edit_cancel()">Cancel</button>
+                                            </div>
+                                            <p id="old_comment" class="" data-id="<?php echo "p" . $comment_id ?>"><?php echo $comment_content ?></p>
+                                        </div>
+                                        <div class="d-flex justify-content-between">
+                                            <div>
+                                                <span class="small"><button class="link-button" id="edit_comment" onclick="edit_comment(this.value)" value="<?php echo $comment_id ?>" data-id="<?php echo "edit_btn" . $comment_id ?>">Edit</button></span>
+                                                <span class="small"><a id="delete_comment" href="delete_comment.php<?php echo '?id=' . $comment_id ?>" class="link" data-id="<?php echo "delete_btn" . $comment_id ?>">Delete</a></span>
+                                            </div>
+                                            <span class="small" data-id="<?php echo "date" . $comment_id ?>"><?php echo $comment_date ?></span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            <?php } ?>
+                            <!-- add a comment -->
+                            <form action="add_comment.php" method="POST">
+                                <div class="row border justify-content-center py-3 my-3 mx-1">
+                                    <div class="col-lg-10  col-md-8  pe-1">
+                                        <textarea class="form-control" name="comment" rows="2" placeholder="Enter a comment"></textarea>
+                                        <input type="text" name="post_id" value="<?php echo $post_id ?>" hidden>
+                                    </div>
+                                    <div class="col-lg-2 col-md-4 mt-2">
+                                        <button type="submit" class="btn btn-secondary">Comment</button>
+                                    </div>
+                            </form>
+                        </div>
+                    </div>
+            </div>
+        <?php } ?>
         </div>
+    </div>
     </div>
 
 
